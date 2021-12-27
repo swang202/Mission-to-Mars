d75d2807eae66a699ab1b885c449f00bb3d2493c
swang202 <70294941+swang202@users.noreply.github.com>
2021-12-27 01:03:18 -0800

    app.py

--- app.py
+++ app.py
@@ -20,7 +20,5 @@ def scrape():
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 
-mars.update_one(query_parameter, {"$set": data}, options)
-
 if __name__ == "__main__":
    app.run()
